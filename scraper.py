74ac5086468ee83d625ab2ac545b695da0ec0d38
Ethan Thomas <ethan021@me.com>
2023-04-25 17:31:35 -0700

    fixes

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, Integer, String, DateTime, Float, Boolean
 from sqlalchemy.orm import sessionmaker
 from dateutil.parser import parse
 from util import post_listing_to_slack, find_points_of_interest
-from slackclient import SlackClient
+# from slackclient import SlackClient
 import time
 import settings
 
@@ -52,60 +52,61 @@ def scrape_area(area):
     while True:
         try:
             result = next(gen)
+            print(result)
         except StopIteration:
             break
         except Exception:
             continue
-        listing = session.query(Listing).filter_by(cl_id=result["id"]).first()
+        # listing = session.query(Listing).filter_by(cl_id=result["id"]).first()
 
         # Don't store the listing if it already exists.
-        if listing is None:
-            if result["where"] is None:
-                # If there is no string identifying which neighborhood the result is from, skip it.
-                continue
+        # if listing is None:
+        #     if result["where"] is None:
+        #         # If there is no string identifying which neighborhood the result is from, skip it.
+        #         continue
 
-            lat = 0
-            lon = 0
-            if result["geotag"] is not None:
-                # Assign the coordinates.
-                lat = result["geotag"][0]
-                lon = result["geotag"][1]
+        #     lat = 0
+        #     lon = 0
+        #     if result["geotag"] is not None:
+        #         # Assign the coordinates.
+        #         lat = result["geotag"][0]
+        #         lon = result["geotag"][1]
 
-                # Annotate the result with information about the area it's in and points of interest near it.
-                geo_data = find_points_of_interest(result["geotag"], result["where"])
-                result.update(geo_data)
-            else:
-                result["area"] = ""
-                result["bart"] = ""
+        #         # Annotate the result with information about the area it's in and points of interest near it.
+        #         geo_data = find_points_of_interest(result["geotag"], result["where"])
+        #         result.update(geo_data)
+        #     else:
+        #         result["area"] = ""
+        #         result["bart"] = ""
 
-            # Try parsing the price.
-            price = 0
-            try:
-                price = float(result["price"].replace("$", ""))
-            except Exception:
-                pass
+        #     # Try parsing the price.
+        #     price = 0
+        #     try:
+        #         price = float(result["price"].replace("$", ""))
+        #     except Exception:
+        #         pass
 
-            # Create the listing object.
-            listing = Listing(
-                link=result["url"],
-                created=parse(result["datetime"]),
-                lat=lat,
-                lon=lon,
-                name=result["name"],
-                price=price,
-                location=result["where"],
-                cl_id=result["id"],
-                area=result["area"],
-                bart_stop=result["bart"]
-            )
+        #     # Create the listing object.
+        #     listing = Listing(
+        #         link=result["url"],
+        #         created=parse(result["datetime"]),
+        #         lat=lat,
+        #         lon=lon,
+        #         name=result["name"],
+        #         price=price,
+        #         location=result["where"],
+        #         cl_id=result["id"],
+        #         area=result["area"],
+        #         bart_stop=result["bart"]
+        #     )
 
-            # Save the listing so we don't grab it again.
-            session.add(listing)
-            session.commit()
+        #     # Save the listing so we don't grab it again.
+        #     session.add(listing)
+        #     session.commit()
 
-            # Return the result if it's near a bart station, or if it is in an area we defined.
-            if len(result["bart"]) > 0 or len(result["area"]) > 0:
-                results.append(result)
+        #     # Return the result if it's near a bart station, or if it is in an area we defined.
+        #     if len(result["bart"]) > 0 or len(result["area"]) > 0:
+        #         results.append(result)
 
     return results
 
@@ -115,15 +116,15 @@ def do_scrape():
     """
 
     # Create a slack client.
-    sc = SlackClient(settings.SLACK_TOKEN)
+    # sc = SlackClient(settings.SLACK_TOKEN)
 
     # Get all the results from craigslist.
     all_results = []
     for area in settings.AREAS:
         all_results += scrape_area(area)
 
-    print("{}: Got {} results".format(time.ctime(), len(all_results)))
+    # print("{}: Got {} results".format(time.ctime(), len(all_results)))
 
     # Post each result to slack.
-    for result in all_results:
-        post_listing_to_slack(sc, result)
+    # for result in all_results:
+    #     post_listing_to_slack(sc, result)
